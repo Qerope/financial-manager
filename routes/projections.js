346d6728486112f7542397b8b1dee7f1f1805908
Qerope <preloopadm@gmail.com>
2025-04-04 22:22:31 -0400

    update

--- routes/projections.js
+++ routes/projections.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { getAverageMonthlyData, getCategoryAverages } from "../controllers/projections.js"
-import { protect } from "../middleware/auth.js"
+import { verifyToken } from "../middleware/auth.js"
 
 const router = express.Router()
 
 // Apply auth middleware to all routes
-router.use(protect)
+router.use(verifyToken)
 
 // Get average monthly income and expenses
 router.get("/average-monthly", getAverageMonthlyData)
