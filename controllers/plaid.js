1025d05e8c9170fb0049b8ea0820b2bb9ee27117
Qerope <preloopadm@gmail.com>
2025-04-08 15:58:12 -0400

    a

--- controllers/plaid.js
+++ controllers/plaid.js
@@ -83,6 +83,12 @@ export const exchangePublicTokenController = async (req, res, next) => {
         item: existingItem,
       })
     } else {
+      console.log(JSON.stringify(PlaidItemSchema.obj, null, 2));
+      console.log("PlaidItem schema for 'accounts':", PlaidItem.schema.path("accounts"));
+      console.log("PlaidItem schema for 'accounts.0':", PlaidItem.schema.path("accounts.0"));
+      console.log("Is 'accounts' an array?", Array.isArray(PlaidItemSchema.obj.accounts));
+      console.log("First account shape:", PlaidItemSchema.obj.accounts[0]);
+      
       // Create a new Plaid item
       const newPlaidItem = new PlaidItem({
         userId,
